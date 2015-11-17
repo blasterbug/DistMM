b735fd175462097887012385f1ecc4b7041df844
blasterbug <blasterbug@users.noreply.github.com>
2015-11-17 16:30:50 +0100

    REST/REST test

---
+++ src/test/java/se/umu/cs/ht15/ens15bsf/TestClientSOAP.java
@@ -0,0 +1,18 @@
+package se.umu.cs.ht15.ens15bsf;
+
+import se.umu.cs.ht15.ens15bsf.soap.MessageAccessorServiceStub;
+
+import java.rmi.RemoteException;
+
+/**
+ * Created by ens15bsf on 2015-11-17.
+ */
+public class TestClientSOAP
+{
+  public static void main( String[] args ) throws RemoteException
+  {
+    MessageAccessorServiceStub client = new MessageAccessorServiceStub(  );
+    MessageAccessorServiceStub.ListOfString topicsString = client.listTopics();
+    System.out.println(topicsString.toString());
+  }
+}
